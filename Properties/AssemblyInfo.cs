1eb4a36e63f8ae01b0fbcc44e2e0bb2e47372f52
Skyue <104967297+Skyue1@users.noreply.github.com>
2022-07-26 16:17:56 +0900

    info for gdbot

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyTitle("xBot Pro UI")]
+[assembly: AssemblyDescription("Graphical User Interface Edition of xBot (2.21)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AndxArtZ")]
+[assembly: AssemblyProduct("xBot Pro UI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("xBot")]
+[assembly: ComVisible(false)]
+[assembly: Guid("82017796-0f20-44f3-90c0-248a0651f232")]
+[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("2.2.1.0")]
